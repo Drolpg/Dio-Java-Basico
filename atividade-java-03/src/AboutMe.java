99a5f954d7f7bb1189d3a6aac3bc1b1cee3091e3
Drolpg <leandronascimento.drolpg21@gmail.com>
2023-04-13 22:16:00 -0300

    atv03

--- atividade-java-03/src/AboutMe.java
+++ atividade-java-03/src/AboutMe.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class AboutMe {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args){
 
         String nome = args [0];
@@ -15,24 +18,24 @@ public class AboutMe {
         System.out.println("Minha altura é " + altura + "cm");
 
 
-        //criando objeto Scanner
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try (//criando objeto Scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Digite seu nome");
+            String userNome = scanner.next();
 
-        System.out.println("Digite seu nome");
-        String userNome = scanner.next();
+            System.out.println("Digite seu sobrenome");
+            String userSobrenome = scanner.next();
 
-        System.out.println("Digite seu sobrenome");
-        String userSobrenome = scanner.next();
+            System.out.println("Digite sua idade");
+            int userIdade = scanner.nextInt();
 
-        System.out.println("Digite sua idade");
-        int userIdade = scanner.nextInt();
+            System.out.println("Digite sua altura");
+            double userAltura  = scanner.nextDouble();
 
-        System.out.println("Digite sua altura");
-        double userAltura  = scanner.nextDouble();
-
-        System.out.println("Ola, me chamo " + userNome + " " + userSobrenome);
-        System.out.println("Tenho " + userIdade + " " + "anos");
-        System.out.println("Minha altura é " + userAltura + "cm");
+            System.out.println("Ola, me chamo " + userNome + " " + userSobrenome);
+            System.out.println("Tenho " + userIdade + " " + "anos");
+            System.out.println("Minha altura é " + userAltura + "cm");
+        }
 
 
 
